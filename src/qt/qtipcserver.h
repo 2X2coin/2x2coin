6671b68ee6727a25bc15af2691f6ccb523bdcd98
michnkeks <37844544+michnkeks@users.noreply.github.com>
2019-09-24 18:51:53 +0200

    welcome 2x2coin
    welcome to 2X2 coin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define 2X2-qt message queue name
+#define BITCOINURI_QUEUE_NAME "2X2URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
